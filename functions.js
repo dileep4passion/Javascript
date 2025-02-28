ba1dd1eea4ee1e48e147cfd76c45214b33fc49f4
Sai Dileep <64729076+dileep4passion@users.noreply.github.com>
2025-02-27 18:31:38 -0600

    add functions notes

---
+++ functions.js
@@ -0,0 +1,39 @@
+//function statement aka function Declaration
+ function a() {
+    console.log("a - function statement");   
+ }
+
+ // difference- hoisting
+ // calling before and after creation
+ // calling b before expression will throw error
+
+ //function expression
+ let b = function (params) {
+    console.log("b - function expression")
+ }
+
+//Anonymous functions
+//no identity
+// used in function expressions
+// used in place functions as values
+//  function () {
+//     console.log("anonymous function")
+//  }
+
+//Named function expressions
+
+let c = function d(){
+    console.log("named function")
+}
+
+//d() gives error
+//reference error 
+
+//parameters vs arguments
+
+//first class functions
+//ability of functions to use and return functions as values 
+
+
+//arrow functions
+
